2eaf9954931db027d36e959f0fdd7a2e803048b4
DiogoCosta2449 <diogo2449@gmail.com>
2025-01-02 16:52:02 +0000

    .

--- Projet_Fisica/Projet_Fisica.ino
+++ Projet_Fisica/Projet_Fisica.ino
@@ -2,11 +2,13 @@
 #include <Arduino_LPS22HB.h>        // Sensor de pressão barométrica
 #include <Arduino_HS300x.h>         // Sensor de temperatura e umidade (HS300x)
 
-float temperature, humidity, pressure, x, y, z;
+// Variáveis globais
+float temperature, humidity, pressure, x, y, z, gx, gy, gz, mx, my, mz;
 unsigned long previousMillis = 0;  // Armazena o último tempo de execução
 const long interval = 2000;        // Intervalo de 2 segundos (2000 ms)
 
-String csvHeader = "Timestamp,Temperature (C),Humidity (%),Pressure (hPa),Acc X,Acc Y,Acc Z\n";
+// Variável para armazenar dados em formato CSV
+String csvHeader = "Timestamp,Temperature (C),Humidity (%),Pressure (hPa),Acc X,Acc Y,Acc Z,Gyro X,Gyro Y,Gyro Z,Mag X,Mag Y,Mag Z\n";
 String csvData = csvHeader;        // Dados em CSV, começando pelo cabeçalho
 
 void setup() {
@@ -49,13 +51,15 @@ void loop() {
       IMU.readAcceleration(x, y, z);
     }
 
-    // Armazenar os dados no formato CSV
-    String row = String(currentMillis / 1000.0) + "," + String(temperature) + "," +
-                 String(humidity) + "," + String(pressure) + "," +
-                 String(x) + "," + String(y) + "," + String(z) + "\n";
-    csvData += row;
+    if (IMU.gyroscopeAvailable()) {
+      IMU.readGyroscope(gx, gy, gz);
+    }
 
-    // Enviar dados ao monitor serial com o formato especificado
+    if (IMU.magneticFieldAvailable()) {
+      IMU.readMagneticField(mx, my, mz);
+    }
+
+    // Exibir dados no monitor serial
     Serial.print("Timestamp: ");
     Serial.print(currentMillis / 1000.0);
     Serial.println(" s");
@@ -84,6 +88,38 @@ void loop() {
     Serial.print(z);
     Serial.println(" m/s²");
 
+    Serial.print("Giroscópio X: ");
+    Serial.print(gx);
+    Serial.println(" º/s");
+
+    Serial.print("Giroscópio Y: ");
+    Serial.print(gy);
+    Serial.println(" º/s");
+
+    Serial.print("Giroscópio Z: ");
+    Serial.print(gz);
+    Serial.println(" º/s");
+
+    Serial.print("Magnetômetro X: ");
+    Serial.print(mx);
+    Serial.println(" µT");
+
+    Serial.print("Magnetômetro Y: ");
+    Serial.print(my);
+    Serial.println(" µT");
+
+    Serial.print("Magnetômetro Z: ");
+    Serial.print(mz);
+    Serial.println(" µT");
+
     Serial.println("-------------------------");
+
+    // Armazenar os dados no formato CSV
+    String row = String(currentMillis / 1000.0) + "," + String(temperature) + "," +
+                 String(humidity) + "," + String(pressure) + "," +
+                 String(x) + "," + String(y) + "," + String(z) + "," +
+                 String(gx) + "," + String(gy) + "," + String(gz) + "," +
+                 String(mx) + "," + String(my) + "," + String(mz) + "\n";
+    csvData += row;
   }
 }
